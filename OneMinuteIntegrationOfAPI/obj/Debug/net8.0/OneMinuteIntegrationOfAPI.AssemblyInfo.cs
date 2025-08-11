93bb192310a4a81d0810429e348981a567d2c411
Vigna.bagdai <Vigna.bagdai@D-SOFTVIGNA>
2025-08-11 18:27:11 +0530

    MXFACESAAS-2279 Prepare a Frontend UI for POC

--- OneMinuteIntegrationOfAPI/obj/Debug/net8.0/OneMinuteIntegrationOfAPI.AssemblyInfo.cs
+++ OneMinuteIntegrationOfAPI/obj/Debug/net8.0/OneMinuteIntegrationOfAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneMinuteIntegrationOfAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24c5a99f06ce61cb39b2dc74f1fc1e559d6b427")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneMinuteIntegrationOfAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneMinuteIntegrationOfAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
